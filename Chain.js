6494c90ac0db4710dd48aac916b71b7d0f4353ae
ismailrcs <mi13arthaz@gmail.com>
2020-09-24 19:48:50 +0530

    sling

--- Chain.js
+++ Chain.js
@@ -1,20 +1,30 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
+       
+       this.pointB = pointB;
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
 
+    Fly(){
+
+     this.chain.bodyA = null ;
+
+    }
+
     display(){
+        if(this.chain.bodyA){
+        
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
     }
-    
+}
 }
